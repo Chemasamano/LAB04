1d9dfd8e211dd05cb6067566041d4f4b9e2177df
Chemamac <chemamac@Chemamacs-Mac-mini.local>
2015-02-06 00:33:16 -0600

    Initial Commit

---
+++ Pushometro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pushometro
+//
+//  Created by Chemamac on 2/6/15.
+//  Copyright (c) 2015 Chemamac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Pushometro/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Pushometro
+//
+//  Created by Chemamac on 2/6/15.
+//  Copyright (c) 2015 Chemamac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
